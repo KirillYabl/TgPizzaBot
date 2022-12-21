d53caac0a93251e7041716c98ca72bb104f85f34
KirillYabl <kirill-yablunovskii@mail.ru>
2022-12-21 16:03:21 +0300

    Fix bug with cart open

--- not_only_one_state_functions.py
+++ not_only_one_state_functions.py
@@ -9,7 +9,7 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.update import Update
 
 import motlin_api
-from singletons import access_keeper
+from singletons.access_keeper import access_keeper
 
 logger = logging.getLogger(__name__)
 
